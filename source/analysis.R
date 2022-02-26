3ede39bdcc0570c2083fa5cb451eaf14dd599a28
IsaWen <wenyuj2@uw.edu>
2022-02-25 23:20:40 -0800

    hw3

--- source/analysis.R
+++ source/analysis.R
@@ -7,10 +7,11 @@
 #  5, What is the standard division of the data?
 
 library("dplyr")
+library("maps")
 library ("ggplot2")
+library("usmap")
 library ("plotly")
 library("leaflet")
-library("rjson")
 
 incarceration <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
 
@@ -31,13 +32,15 @@ time_min_total_jail_pop <- incarceration%>%
   filter(total_jail_pop == min_total_jail_pop)%>%
   pull(year)
 
-# the changes of the value of total population in jail from 2008-2018
-total_pop_10 <- incarceration %>%
-  filter(year >= 2008 & year <= 2018)
-max_total_pop_10 <- max(total_pop_10$total_jail_pop,na.rm = TRUE)
-min_total_pop_10 <- min(total_pop_10$total_jail_pop,na.rm = TRUE)
+# the changes of the value of total population in jail between 2008 and 2018
+total_pop_08 <- incarceration %>%
+  filter(year == 2008 )
+total_pop_18 <- incarceration %>%
+  filter(year == 2018 )
+max_total_pop_08 <- max(total_pop_08$total_jail_pop,na.rm = TRUE)
+max_total_pop_18 <- max(total_pop_18$total_jail_pop,na.rm = TRUE)
 
-change_total_jail_pop <- max_total_pop_10 - min_total_pop_10
+change_total_jail_pop <- max_total_pop_08 - max_total_pop_18
 
 # the standard division of the value of total population from in jail
 SD_total_jail_pop <- sd(incarceration$total_jail_pop,na.rm = TRUE)
@@ -66,13 +69,15 @@ time_min_aapi_jail_pop_base <- incarceration%>%
   pull(year)
 time_min_aapi_jail_pop <- unique(time_min_aapi_jail_pop_base)
 
-# the changes of the value of aapi population in jail from 2008-2018
-aapi_pop_10 <- incarceration %>%
-  filter(year >= 2008 & year <= 2018)
-max_aapi_pop_10 <- max(aapi_pop_10$aapi_jail_pop,na.rm = TRUE)
-min_aapi_pop_10 <- min(aapi_pop_10$aapi_jail_pop,na.rm = TRUE)
+# the changes of the value of aapi population in jail between 2008 and 2018
+aapi_pop_08 <- incarceration %>%
+  filter(year == 2008 )
+aapi_pop_18 <- incarceration %>%
+  filter(year == 2018 )
+max_aapi_pop_08 <- max(aapi_pop_08$aapi_jail_pop,na.rm = TRUE)
+max_aapi_pop_18 <- max(aapi_pop_18$aapi_jail_pop,na.rm = TRUE)
 
-change_aapi_jail_pop <- max_aapi_pop_10 - min_aapi_pop_10
+change_aapi_jail_pop <- max_aapi_pop_08 - max_aapi_pop_18
 
 # the standard division of the value of aapi population from in jail
 SD_aapi_jail_pop <- sd(incarceration$aapi_jail_pop,na.rm = TRUE)
@@ -95,13 +100,15 @@ time_min_black_jail_pop <- incarceration%>%
   filter(black_jail_pop == min_black_jail_pop)%>%
   pull(year)
 
-# the changes of the value of black population in jail from 2008-2018
-black_pop_10 <- incarceration %>%
-  filter(year >= 2008 & year <= 2018)
-max_black_pop_10 <- max(black_pop_10$black_jail_pop,na.rm = TRUE)
-min_black_pop_10 <- min(black_pop_10$black_jail_pop,na.rm = TRUE)
+# the changes of the value of black population in jail between 2008 and 2018
+black_pop_08 <- incarceration %>%
+  filter(year == 2008 )
+black_pop_18 <- incarceration %>%
+  filter(year == 2018 )
+max_black_pop_08 <- max(black_pop_08$black_jail_pop,na.rm = TRUE)
+max_black_pop_18 <- max(black_pop_18$black_jail_pop,na.rm = TRUE)
 
-change_black_jail_pop <- max_black_pop_10 - min_black_pop_10
+change_black_jail_pop <- max_black_pop_08 - max_black_pop_18
 
 # the standard division of the value of black population from in jail
 SD_black_jail_pop <- sd(incarceration$black_jail_pop,na.rm = TRUE)
@@ -110,50 +117,20 @@ SD_black_jail_pop <- sd(incarceration$black_jail_pop,na.rm = TRUE)
 
 
 
-# the average value of white population in jail
-mean_white_jail_pop <- mean(incarceration$white_jail_pop,na.rm = TRUE)
-
-# when is the max of the value of white population in jail
-max_white_jail_pop <- max(incarceration$white_jail_pop,na.rm = TRUE)
-time_max_white_jail_pop <- incarceration%>%
-  filter(white_jail_pop == max_white_jail_pop)%>%
-  pull(year)
-
-# when is the min of the value of white population in jail
-min_white_jail_pop <- min(incarceration$white_jail_pop,na.rm = TRUE)
-time_min_white_jail_pop <- incarceration%>%
-  filter(white_jail_pop == min_white_jail_pop)%>%
-  pull(year)
-
-# the changes of the value of white population in jail from 2008-2018
-white_pop_10 <- incarceration %>%
-  filter(year >= 2008 & year <= 2018)
-max_white_pop_10 <- max(white_pop_10$white_jail_pop,na.rm = TRUE)
-min_white_pop_10 <- min(white_pop_10$white_jail_pop,na.rm = TRUE)
-
-change_white_jail_pop <- max_white_pop_10 - min_white_pop_10
-
-# the standard division of the value of white population from in jail
-SD_white_jail_pop <- sd(incarceration$white_jail_pop,na.rm = TRUE)
-
-
-
-
 
 # Trends over time chart
 incarceration_NA <- incarceration %>%
   filter(na.rm = TRUE, 
-         year >= 2000 & year<=2018,
-         region == "West")
+         year >= 2008 & year<=2018,
+         county_name == "King County")
 hist <- ggplot(incarceration_NA) +
-  geom_col(mapping = aes(x = year, y = white_jail_pop), position = "dodge") +
+  geom_line(mapping = aes(x = year, y = white_jail_pop,color = state)) +
   labs(
     title = "white people population in jail depends on year in King county",
     x = "time in year",
     y = "white people population in jail",
   )
 ggplotly(hist)
-
 
 #Variable comparison chart
 scatter <- ggplot(incarceration_NA) +
@@ -166,43 +143,15 @@ scatter <- ggplot(incarceration_NA) +
 ggplotly(scatter)
 
 # Map
-url <- 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
-counties <- rjson::fromJSON(file=url)
-
 incarceration_2018 <- incarceration %>%
   filter(na.rm = TRUE, 
          year==2018)
 
-g <- list(
-  scope = 'usa',
-  projection = list(type = 'albers usa'),
-  showlakes = TRUE,
-  lakecolor = toRGB('white')
-)
+map <- plot_usmap(data = incarceration_2018, values = "black_jail_pop") + 
+  scale_fill_continuous(name = "Population in jail (2018)", label = scales::comma) + 
+  theme(legend.position = "right")+labs(title = "2018 distribution of black population in jail")
 
-fig <- plot_ly()
-fig <- fig %>% add_trace(
-  type="choropleth",
-  geojson=counties,
-  locations = incarceration_2018$fips,
-  z=incarceration_2018$black_jail_pop,
-  colorscale="Viridis",
-  marker=list(line=list(
-    width=0)
-  )
-)
-fig <- fig %>% colorbar(title = "population black people in jail in 2018")
-fig <- fig %>% layout(
-  title = "2018 distribution of black population in jail"
-)
-fig <- fig %>% layout(
-  geo = g
-)
-
-fig
-
-
-
+map
 
 
 
